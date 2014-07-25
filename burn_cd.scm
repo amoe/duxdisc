d24aa50ce55b2f8482bc70ca42bea8c5c6acf98e
David Banks <amoebae@gmail.com>
2014-07-25 08:01:35 +0100

    adding a new 'measure' action

--- burn_cd.scm
+++ burn_cd.scm
@@ -78,10 +78,15 @@
   (let loop ((path-so-far root)
 	     (tree (file-system-tree root))
 	     (in-root? #t))
+    (when (not tree)
+      (error "Something strange happened -- root dir probably doesn't exist" root tree))
+
     (let ((full-path (if in-root?
 			 path-so-far
 			 (string-append path-so-far "/" (first tree)))))
       (cond
+       ((not tree)
+	(error "can't happen" tree root in-root? path-so-far))
        ((= (length tree) 2)
 	(if (is-audio-file? full-path)
 	    (list full-path)
@@ -132,15 +137,25 @@
     (cond
      ((null? items)  (values burn-list remove-list))
      (else
+      (display  "Decoding and fixing item ")
+      (write (car items))
+      (newline)
       (let-values (((burn remove) (decode-and-fix-item (car items))))
 	(loop (cdr items) (append burn-list burn) (append remove-list remove)))))))
 
 (define (decode-and-fix-item name)
+  (display "About to maybe unzip\n")
   (let ((item (maybe-unzip name)))
+    (display "About to scan tree\n")
+    (display "Will scan item ")
+    (write item)
+    (newline)
     (let ((files (scan-tree item)))
+      (display "About to decode\n")
       (let ((decoded (map decode files)))
 	  (let ((fixed (map fix-format files)))
 	    (write fixed)
+	    (newline)
 	    (values (map (lambda (original-path)
 			   (original-path->output-path original-path "resample" ".wav"))
 			 (sort files string<))
@@ -153,7 +168,7 @@
   (let ((command (string-append "rm -rf " (shell-quote path))))
     (system/wrapped command)))
   
-(define (main . args)
+(define (action:burn . args)
   (let-values (((burn-list remove-list) (decode-and-merge args)))
     (let ((count (exact->inexact (milliseconds->minutes (count-files burn-list)))))
       (when (> count 80)
@@ -164,6 +179,41 @@
       (burn burn-list)
       (for-each recursive-delete remove-list))))
 
+(define (action:burn-incoming . args)
+  (let-values (((burn-list remove-list) (decode-and-merge args)))
+    (let ((count (exact->inexact (milliseconds->minutes (count-files burn-list)))))
+      (when (> count 80)
+        (error "too large for disc" count))
+      (when (< count 40)
+        (error "too small for disc" count))
+
+      (burn burn-list))))
+
+
+(define (main . args)
+  (when (< (length args) 1)
+    (error "no action specified"))
+
+  (let ((action (first args)))
+    (cond
+     ((string=? action "burn")
+      (apply action:burn (cdr args)))
+     ((string=? action "burn-incoming")
+      (apply action:burn-incoming (cdr args)))
+     ((string=? action "measure")
+      (apply action:measure (cdr args)))
+     (else
+      (error "unknown action" action)))))
+
+(define (action:measure . args)
+  (let-values (((burn-list remove-list) (decode-and-merge args)))
+    (let ((count (exact->inexact (milliseconds->minutes (count-files burn-list)))))
+      (display "Length of disc will be ")
+      (display count)
+      (display "m.")
+      (newline))))
+
+  
 (define (main2)
   (let ((usages (map (lambda (dir)
 		       (cons dir (disk-usage dir)))
